eef3b980524ba95f24081c9b05914f17d0c92afa
hashim <j_hashim1305@outlook.com>
2022-11-04 14:52:47 +0800

    start button is functional

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
-//getting all the required elements
-const start_btn = document.querySelector(".start_btn button");
-const info_box = document.querySelector(".info_box");
-const exit_btn = info_box.querySelector(".buttons .quit");
-const continue_btn = info_box.querySelector(".buttons .restart");
-const quiz_box = document.querySelector(".quiz_box");
+const btnStartGame = document.getElementById('button-start-game');
+const sectionLanding = document.getElementById('section-landing');
+const sectionQuestion = document.getElementById('section-question');
 
-//If Start Quiz Button Clicked
-start_btn.onclick = ()=>{
-    info_box.classList.add("activeInfo"); //show the info box
-}
+//when start button is clicked:
+btnStartGame.addEventListener('click', function(event){
+// hide the landing page
+   sectionLanding.classList.add('hide');
+//show the question section 
+   sectionQuestion.classList.remove('hide');
+});
 
-//If Exit Button Clicked
-exit_btn.onclick = ()=>{
-    info_box.classList.remove("activeInfo"); //hide the info box
-}
 
-//If Continue Button Clicked
-continue_btn.onclick = ()=>{
-    info_box.classList.remove("activeInfo"); //hide the info box
-    quiz_box.classList.add("activeQuiz"); //show the quiz box
-}
+
+//once in the question section;
+// start timer
+
+//when user clicks on on a answer
+//give feedback (right/wrong)
+
+//if correct: tell them they are correct
+//if wrong: tell them they are wrong & deduct 10s off timer 
+
+//if timeis less than 0 end game
+//end game - show the endgame screen
+//stop timer
+//show score
+//hide the question section
+
+// if time is more than 0 next question
+
+// if user clicks on an answer on last question, end game
+
+//once on end game screen show score and have initial imput and submit
+//if user clicks submit with no imput stop user from procieding, and alert them to type in imput
+//if user types in imput and submit, store score asnd imput in local storage
+//then show the high score page
+//hide end game screen
+// show high score page
+//show all previous highscores
+//grab data from local storage
+//order them from high to low
+
+// if user clicks on retry btn, take them to landing page
+//hide highscore page, and show landing page
+
+
+
